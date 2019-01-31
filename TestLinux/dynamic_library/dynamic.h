c545f19a88f78e94a491177b74189f46af5543e9
zhaojf <zhaojf_2009@163.com>
2019-01-31 15:04:06 +0800

    new build

---
+++ TestLinux/dynamic_library/dynamic.h
@@ -0,0 +1,6 @@
+
+#ifndef dynamic_h__
+#define dynamic_h__
+
+int dynamic_test(int n);
+#endif // dynamic_h__
---
+++ TestLinux/static_library/static.h
@@ -0,0 +1,5 @@
+#ifndef static_h__
+#define static_h__
+
+int static_test(int n);
+#endif // static_h__
